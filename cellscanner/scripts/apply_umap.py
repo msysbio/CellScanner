626f1d3c17112f5d544c2b925ab457b182e24263
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2025-03-31 18:39:38 +0200

    2.0.1 docker

--- cellscanner/scripts/apply_umap.py
+++ cellscanner/scripts/apply_umap.py
@@ -118,15 +118,14 @@ def process_files(TrainPanel: "TrainModelPanel" = None, **kwargs):
             "working_directory": TrainPanel.file_panel.working_directory,
         }
 
-        gating = TrainPanel.gating_checkbox.isChecked()
-
-        # if gating:
+        # NOTE (Haris Zafeiropoulos, 2025-03-31):
+        # If gating is not been clicked at all, stain_1 and stain_2 will be blank stains
         stain_1, stain_2 = get_stains_from_panel(TrainPanel)
-        # else:
-        #     stain_1 = stain_2 = Stain(channel=None, sign=None, value=None)
+
         gui = True
 
     else:
+
         # Read parameters from kwargs
         required_keys = [
             "n_events", "umap_n_neighbors", "umap_min_dist",
